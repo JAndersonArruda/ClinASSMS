e9675eda716971ae63bbe37f8059b5a642dce52c
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2025-01-04 10:39:52 -0300

    adding server's structure

---
+++ server/src/main/java/com/clinassms/server/ClinassmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.clinassms.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinassmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinassmsServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/clinassms/server/ClinassmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinassms.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinassmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
